836a09a1823964ff1b14f86a7d8d6fa1227a0498
Ewocker <chockerlin@gmail.com>
2023-03-26 00:00:11 -0700

    same

--- layouts/blog.vue
+++ layouts/blog.vue
@@ -20,18 +20,23 @@ for (let page of data.value!) {
            :alt="page.image.alt"
            class="mt-0 hero w-full" />
     </div>
-    <div class="flex flex-wrap px-10">
-      <div class="flex flex-wrap w-full lg:w-2/3 px-10">
+    <div class="flex flex-wrap px-3">
+      <div class="flex flex-wrap w-full lg:w-2/3 md:px-10 justify-center item-center">
         <div class="w-full font-family-edu text-5xl mb-5 underline font-semibold text-orange-800">
           <i>Story</i>
         </div>
         <TocCard v-for="page in data"
                  :key="'serie' + page._id"
                  :page="page"
-                 class="w-full not-prose" />
+                 class="w-full not-prose hidden md:flex" />
+
+        <PostCard v-for="page in data"
+                  :key="'serie' + page._id"
+                  :page="page"
+                  class="w-full not-prose block md:hidden my-20" />
       </div>
 
-      <div class="w-full lg:w-1/3 px-10">
+      <div class="w-full lg:w-1/3 md:px-10">
         <div class="w-full font-family-edu text-5xl mb-5 underline font-semibold text-orange-800">
           <i>Tag</i>
         </div>
