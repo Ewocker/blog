d1ccd1b2f7e72435a6f50524724b862af912f60e
beverly <byuhuang@ucdavis.edu>
2023-11-27 20:50:26 -0800

    add yutong as an author and add 3 posts

--- components/BlogAuthor/data.ts
+++ components/BlogAuthor/data.ts
@@ -2,15 +2,22 @@ export interface Author {
   name: string
   image: string
   url: string
-  facebook: string
+  profileUrl: string
 }
 const authors: Map<string, Author> = new Map()
 
 authors.set('小貓貓工程師', {
   name: '小貓貓工程師',
   image: '/common/siliconvalleykittycatengineer.jpeg',
-  url: '/about',
-  facebook: 'https://www.facebook.com/SiliconValleyKittyCatEngineer',
+  url: '/author/siliconvalleykittycatengineer',
+  profileUrl: 'https://www.facebook.com/SiliconValleyKittyCatEngineer',
+})
+
+authors.set('小喵喵数据分析师', {
+  name: '小喵喵数据分析师',
+  image: '/common/siliconvalleykittycatdatascientist.jpeg',
+  url: '/author/siliconvalleykittycatdatascientist',
+  profileUrl: 'https://www.linkedin.com/in/yutonghuang/',
 })
 
 export default authors
